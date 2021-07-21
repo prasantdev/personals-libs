4b6b199d982df11f45820547118305d15a340b3b
Prasant <67827472+prasantdev@users.noreply.github.com>
2021-07-21 13:11:39 +0530

    Update index.js

--- owlcred/index.js
+++ owlcred/index.js
@@ -1,8 +1,2 @@
-const fetch = require('node-fetch');
-
-fetch(
-    `https://owlbot.info/api/v4/dictionary/apple`,
-    { headers: { Authorization: `Token ${{secrets.OWL_TOKEN}}` } }
-)
-.then(res => res.json())
-.then(r => console.log(r))
+let a = {${secrets.TEST}}
+console.log(a)
